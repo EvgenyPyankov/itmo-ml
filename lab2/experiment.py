c385986a22f8a05a42a8c5db6c23624f56e4fb9f
Евгений <Sleepwalker17@mail.ru>
2017-10-15 21:39:34 +0300

    Added Data class

--- lab2/experiment.py
+++ lab2/experiment.py
@@ -1,16 +1,14 @@
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
 from utils import print_results
 
 
 class Experiment:
-    def __init__(self, num, train_percentage, data):
-        assert train_percentage < 1
+    def __init__(self, num, data):
         self.num = num
-        self.train_percentage = train_percentage
-        self.train, self.test = train_test_split(data, train_size=train_percentage)
+        self.data = data
         self.dtc = DecisionTreeClassifier()
         self.rfc = RandomForestClassifier()
 
@@ -19,19 +17,16 @@ class Experiment:
                'Training percentage = {:0.0f}%\n' \
                'Dicision Tree Classifier accuracy = {:0.2f}%\n' \
                'Randrom Forest Classifier accuracy = {:0.2f}%\n\n'.format(self.num,
-                                                                          self.train_percentage * 100,
+                                                                          self.data.train_size * 100,
                                                                           dtc_accuracy * 100,
                                                                           rfc_accuracy * 100)
         print_results(text)
 
     def run(self):
-        x_attributes = ['Slength', 'Swidth', 'Plength', 'Pwidth']
-        X = self.train.loc[:, x_attributes]
-        Y = self.train['Class']
-        self.dtc.fit(X, Y)
-        self.rfc.fit(X, Y)
-        dtc_predict = self.dtc.predict(self.test.loc[:, x_attributes])
-        rfc_predict = self.rfc.predict(self.test.loc[:, x_attributes])
-        dtc_accuracy = accuracy_score(self.test['Class'], dtc_predict)
-        rfc_accuracy = accuracy_score(self.test['Class'], rfc_predict)
+        self.dtc.fit(self.data.train_x, self.data.train_y)
+        self.rfc.fit(self.data.train_x, self.data.train_y)
+        dtc_predict = self.dtc.predict(self.data.test_x)
+        rfc_predict = self.rfc.predict(self.data.test_x)
+        dtc_accuracy = accuracy_score(self.data.test_y, dtc_predict)
+        rfc_accuracy = accuracy_score(self.data.test_y, rfc_predict)
         self.print_results(dtc_accuracy, rfc_accuracy);
--- lab2/main.py
+++ lab2/main.py
@@ -1,14 +1,15 @@
 import pandas as pnd
+from data import Data
 from lab2.experiment import Experiment
 
 data = pnd.read_csv('../data/iris.csv')
 
 open('docs/results.txt', 'w').close()
 
-exp1 = Experiment(1, 0.6, data)
-exp2 = Experiment(2, 0.7, data)
-exp3 = Experiment(3, 0.8, data)
-exp4 = Experiment(4, 0.9, data)
+exp1 = Experiment(1, Data(data, 0.6))
+exp2 = Experiment(2, Data(data, 0.7))
+exp3 = Experiment(3, Data(data, 0.8))
+exp4 = Experiment(4, Data(data, 0.9))
 exp1.run()
 exp2.run()
 exp3.run()
